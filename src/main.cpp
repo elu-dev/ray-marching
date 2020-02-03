ad7c604cbea6f0e8dc84b101d2778c3834aa6818
) <)>
2020-02-04 00:35:34 +0100

    add msg handlers for exit and keyboard events

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 
 #include <windows.h>
 
+LRESULT CALLBACK WindProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_CLOSE: PostQuitMessage(69); break;
+        case WM_KEYDOWN:
+            if (wParam == 'F') SetWindowText(hwnd, "Respects");
+            break;
+        case WM_KEYUP:
+            if (wParam == 'F') SetWindowText(hwnd, "DangerField");
+            break;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     
     const auto pClassName = "hw3dbutts";
@@ -9,7 +22,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     WNDCLASSEX wc = {0};
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WindProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -29,11 +42,19 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         200, 200, 640, 480,
         nullptr, nullptr, hInstance, nullptr
     );
-
+    
+    // show the damn window
     ShowWindow(hwnd, SW_SHOW);
 
-    while (true);
-    
-    return 0;
+
+    // message pump
+    MSG msg;
+    BOOL gResult;
+    while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    if (gResult == -1) return -1;
+    else return msg.wParam;
 }
 
